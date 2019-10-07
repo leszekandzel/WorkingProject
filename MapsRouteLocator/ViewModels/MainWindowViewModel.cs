6d70e78b32e6b4dcd42760f22813be43fa672d4a
Leszek Andzel <leszek.andzel@gmail.com>
2019-10-07 17:46:52 +0200

    First commit

---
+++ MapsRouteLocator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace MapsRouteLocator.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title
+        {
+            get { return "This is a title"; }
+        }
+    }
+}
