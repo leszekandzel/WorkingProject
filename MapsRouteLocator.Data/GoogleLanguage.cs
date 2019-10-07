1fbd21903f81bbc6a9c35c9fb79d49ca81771a3b
Leszek Andzel <leszek.andzel@gmail.com>
2019-10-07 18:26:46 +0200

    Added GoogleLanguageLocator

---
+++ MapsRouteLocator.Data/GoogleLanguage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapsRouteLocator.Data
+{
+    /// <summary>
+    /// Language to pass to Google service in order to receive data in selected language.
+    /// </summary>
+    public class GoogleLanguage
+    {
+        private string code;
+        private string name;
+
+        public GoogleLanguage(string code, string name)
+        {
+            this.code = code;
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Code to pass to Google service in order to receive data in selected language.
+        /// </summary>
+        public string  Code { get;  }
+
+        /// <summary>
+        /// Name just for better way do understand which language it is.
+        /// </summary>
+        public string Name { get;  }
+    }
+}
