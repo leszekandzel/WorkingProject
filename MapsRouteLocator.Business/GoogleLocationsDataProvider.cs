7aedb0b0fb241f293911876252e082b5e094e247
Leszek Andzel <leszek.andzel@gmail.com>
2019-10-08 21:38:07 +0200

    GetLocationsList primitive implementation

--- MapsRouteLocator.Business/GoogleLocationsDataProvider.cs
+++ MapsRouteLocator.Business/GoogleLocationsDataProvider.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using MapsRouteLocator.Data;
 using MapsRouteLocator.Interfaces;
+using System.Data.Linq;
 
 namespace MapsRouteLocator.Business
 {
@@ -19,7 +24,42 @@ namespace MapsRouteLocator.Business
         public IEnumerable<LocationData> GetLocationsList(string prefix)
         {
             var queryString = this.googleLocationsQueryProvider.GetLocationsQuery(prefix);
-            return null;
+
+            DataSet ds = new DataSet();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(queryString);
+            WebResponse response = request.GetResponse();
+            try
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader sreader = new StreamReader(dataStream);
+                string responsereader = sreader.ReadToEnd();
+                ds.ReadXml(new XmlTextReader(new StringReader(responsereader)));
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                response.Close();
+            }
+
+            var status = ds.Tables["FindPlaceFromTextResponse"].Rows[0]["status"];
+            var candidates = ds.Tables["candidates"];
+
+            if (status.ToString().ToLower() != "ok")
+            {
+                return null;
+            }
+
+            if (candidates.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            var results = candidates.AsEnumerable().Select(x => new LocationData() {Name = x.Field<string>("name")});
+
+            return results;
         }
     }
 }
