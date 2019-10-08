85462080834d578fd6b4492e8a320b99e016728a
Leszek Andzel <leszek.andzel@gmail.com>
2019-10-08 09:54:27 +0200

    Dynamically added navigation vias

--- MapsRouteLocator/ViewModels/NavigationPanelViewModel.cs
+++ MapsRouteLocator/ViewModels/NavigationPanelViewModel.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace MapsRouteLocator.ViewModels
 {
-    class NavigationPanelViewModel : BindableBase
+    public class NavigationPanelViewModel : BindableBase
     {
+        private ObservableCollection<int> routes;
+        public ICommand AddNewRouteCommand { get; }
+        public NavigationPanelViewModel()
+        {
+            this.routes = new ObservableCollection<int>();
+            this.routes.Add(1);
+            this.routes.Add(2);
+            this.AddNewRouteCommand = new DelegateCommand(this.AddNewViewRoute);
+        }
+
+        private void AddNewViewRoute()
+        {
+            this.routes.Add(0);
+        }
+
+
+        public ObservableCollection<int> ViaRoutes
+        {
+            get
+            {
+                return routes;
+            }
+        }
+
     }
 }
